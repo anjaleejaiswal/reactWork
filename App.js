65b43b241e4ebeef13ee8f489b3335df6854d3fb
anjaleejaiswal <anjaleejaiz@gmail.com>
2024-08-08 23:29:11 +0530

    episode 1

--- App.js
+++ App.js
@@ -1,4 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",{id:"heading"},"Hello World")
+
+//React Element
+//const heading = React.createElement("h1",{},"Hello");
+const elem = <span>inner span</span>
+const heading = <h1>This is react!!</h1>
+
+//or 
+
+const heading2 = (
+    <div>
+        <h1>Test heading 2</h1>
+        {elem}
+    </div>
+);
+
+//React functional component
+const HeadComponent = () => <h1>Test headComponent</h1>
+
+//or
+
+const HeadComponent2 = () => (
+    <h1 className="test">Test 2</h1>
+);
+
+//or
+
+const num = 1000;
+const HeadComponent3 = () => {
+    return (
+        <div className="container">
+            <div>Test HeadComponent3</div>
+            <HeadComponent2/>
+            {HeadComponent()}
+            {heading2}
+            {num}
+            {console.log("abcd")}
+        </div>
+    )
+};
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+//root.render(heading2);
+
+root.render(<HeadComponent3/>)
---
+++ README.md
@@ -0,0 +1,21 @@
+#namaste react
+
+
+#Parcel
+Dev Build 
+Local Server
+HMR (Hot Module Replacement) - it refreshes automatically
+File Watching Algorithm - written in c++
+Caching - Faster Builds
+Image Optimization
+Minification
+Bundle
+Compress
+Consistent Hashing
+Differential Bundling(supports older browser)
+code splitting
+Error Handling
+Tree shaking (remove unused code)
+Diagonistic
+HTTPs
+Different Dev and Prod bundles
